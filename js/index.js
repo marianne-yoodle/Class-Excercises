e93be960ae48d217adebadf752af681c31f1140a
marianne-yoodle <marianne@yoodle.com>
2022-05-19 10:31:11 -0500

    Began JS

--- js/index.js
+++ js/index.js
@@ -1,5 +1,56 @@
-const submit = document.getElementById('submit');
+// var ourFirstVariable
+let ourFirstVariable;
+ourFirstVariable = 'test';
+console.log(ourFirstVariable);
 
-function writeText(){
-    submit.innerHTML = "Got it";
-}
+const ourSecondVariable = 'Huzzah';
+
+ourFirstVariable = 'have';
+ourFirstVariable = `Edison said "I ${ourFirstVariable} seen him."`;
+
+let ourFirstNum = 2;
+let ourSecondNum = 5;
+
+const sumOfNums = ourFirstNum + ourSecondNum;
+
+let ourBoolean = false;
+
+let nameArray = ['Jon', 'Ray', 'Rodrick'];
+
+let car = {
+    color: 'black',
+    wheels: 5,
+    doors: 4,
+    engine: 6,
+    make: 'Ford',
+    model: 'Mustang'
+};
+
+let classObj = [
+    {
+        name: {
+            first: 'Jon',
+            last: 'Wright',
+        },
+        title: 'Instructor'
+    },
+    {
+        name: {
+            first: 'Ray',
+            last: 'Barbosa',
+        },
+        title: 'Student'
+    },
+    {
+        name: {
+            first: 'Rodrick',
+            last: 'Cotton',
+        },
+        title: 'Student'
+    }
+]
+
+let ourFirstArray = ['a string', 'another string', 123, true, nameArray, car, classObj];
+
+let aSentence = classObj[0].name.first + ' ' + classObj[0].name.last;
+let anotherSentence = 'this that';
